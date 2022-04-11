e5b38f18a1a4bedacd56e5b8a9e786c0cfab6c78
Verox <68915259+Verox145@users.noreply.github.com>
2022-04-11 11:47:26 +0200

    aggiungere colonna cipg alla geologia

---
+++ Corso_Python/aggiungere colonna cipg alla geologia.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = [] # lista geologia
+]
+df = pd.DataFrame(data,columns=['sigla','descrizion','descr_chia','gruppi_gi','descr_grup','num_progre','chiave_gi','id']) 
+cipg = [] #questa lista deve popolarla l'utente inserendo un valore di cipg per ogni litologia presente
+df['cipg'] = cipg
+print(df)
